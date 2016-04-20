bd03aa6ed93594c730b5af425ef6333c88586687
Magnus Morris <developermagnus@gmail.com>
2016-04-20 11:18:15 +1000

    webpack + babel + npm config

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const webpack = require('webpack');
+module.exports = {
+  module: {
+    loaders: [
+      { test: /\.jsx?$/, loaders: ['babel-loader'] }
+    ]
+  },
+  entry: [ './app/index.jsx' ],
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  output: {
+    path: __dirname + '/js',
+    filename: 'app.js'
+  }
+};
